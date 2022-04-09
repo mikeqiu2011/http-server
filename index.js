bd3a90f667dca634ab9cf4b762a585cb864fbb5d
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 15:06:53 +0800

    only serve request of url to /friend

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const http = require('http')
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    })
+    if (req.url === '/friends') {
 
-    res.end(JSON.stringify({
-        name: 'mike',
-        age: 30
-    }))
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+
+        res.end(JSON.stringify({
+            name: 'mike',
+            age: 30
+        }))
+    }
 })
 
 server.listen(PORT, () => {
