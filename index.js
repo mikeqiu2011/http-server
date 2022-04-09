6ddbf6f6ed742ff5b30f9430c806a134efa5631f
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 15:10:54 +0800

    write html response primitively

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const PORT = 3000
 
 const server = http.createServer((req, res) => {
     if (req.url === '/friends') {
-
         res.writeHead(200, {
             'Content-Type': 'application/json'
         })
-
         res.end(JSON.stringify({
             name: 'mike',
             age: 30
         }))
+    } else if (req.url === '/messages') {
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1> hihi </h1>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
     }
 })
 
